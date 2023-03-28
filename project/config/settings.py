c1d1e9425abaef1adb7b4cc652b2f9cf3b409304
rovsen762 <rovsen.memmedli.762@gmail.com>
2023-03-29 03:40:39 +0400

    New updates by Chapter 4.
    1.I created new acoount app.
    2.Created login form for sign in to website.Also I created user_login view and url.
    3.Created login.html for get login data from user

--- project/config/settings.py
+++ project/config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -40,7 +41,7 @@ INSTALLED_APPS = [
 
     #Local apps
 
-    'apps.account'
+    'apps.account',
 
     #Third party apps
 ]
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
