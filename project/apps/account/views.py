b85e0cc21f003f04855472b671aa20bd63f4e099
rovsen762 <rovsen.memmedli.762@gmail.com>
2023-03-29 06:45:24 +0400

    New updates by Chapter 4.
    1.Created django built-in auth views like Login and Logout.
    2.Also I created login,dashboard and logout pages for displaying auth processes on browser.
    3.Also I added login,logout routes into django settings file and created new registration file directories in the template file.

--- project/apps/account/views.py
+++ project/apps/account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -21,3 +22,7 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request,'dashboard.html',{'section': 'dashboard'})
--- project/config/settings.py
+++ project/config/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
 
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
